3ca1c1f2dec6813766301095d63ad1cec1cc9838
Gian <g.culaon.lav@gmail.com>
2024-08-12 09:05:49 +0200

    Add project files.

---
+++ Personal Task Manager/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal_Task_Manager.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+
+    }
+}
---
+++ Personal Task Manager/Views/TasksForm/TasksView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Personal_Task_Manager.Views.TasksForm
+{
+    public partial class TasksView : Form
+    {
+        public TasksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
